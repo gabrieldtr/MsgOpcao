bf44c549ce7de5aa7d0d63fb1d19157ef5a2f934
gabrieldtr <100310095+gabrieldtr@users.noreply.github.com>
2022-03-30 08:22:45 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,80 @@
+function executarAcao1(){
+    let valor = 1;
+    switch(valor){
+        case 1: {
+            let div1 = document.getElementById("msg1");
+            div1.classList.remove("filha0");
+            div1.classList.add("filha2");
+            let div2 = document.getElementById("msg2");
+            div2.classList.remove("filha2");
+            div2.classList.add("filha0");
+            let div3 = document.getElementById("msg3");
+            div3.classList.remove("filha2");
+            div3.classList.add("filha0");
+            let div4 = document.getElementById("msg4");
+            div4.classList.remove("filha2");
+            div4.classList.add("filha0");
+            break;
+        }
+    }
+}
+function executarAcao2(){
+    let valor = 2;
+    switch(valor){
+        case 2: {
+            let div1 = document.getElementById("msg1");
+            div1.classList.remove("filha2");
+            div1.classList.add("filha0");
+            let div2 = document.getElementById("msg2");
+            div2.classList.remove("filha0");
+            div2.classList.add("filha2");
+            let div3 = document.getElementById("msg3");
+            div3.classList.remove("filha2");
+            div3.classList.add("filha0");
+            let div4 = document.getElementById("msg4");
+            div4.classList.remove("filha2");
+            div4.classList.add("filha0");
+            break;
+        }
+    }
+}
+function executarAcao3(){
+    let valor = 3;
+    switch(valor){
+        case 3: {
+            let div1 = document.getElementById("msg1");
+            div1.classList.remove("filha2");
+            div1.classList.add("filha0");
+            let div2 = document.getElementById("msg2");
+            div2.classList.remove("filha2");
+            div2.classList.add("filha0");
+            let div3 = document.getElementById("msg3");
+            div3.classList.remove("filha0");
+            div3.classList.add("filha2");
+            let div4 = document.getElementById("msg4");
+            div4.classList.remove("filha2");
+            div4.classList.add("filha0");
+            break;
+        }
+    }
+}
+function executarAcao4(){
+    let valor = 4;
+    switch(valor){
+        case 4: {
+            let div1 = document.getElementById("msg1");
+            div1.classList.remove("filha2");
+            div1.classList.add("filha0");
+            let div2 = document.getElementById("msg2");
+            div2.classList.remove("filha2");
+            div2.classList.add("filha0");
+            let div3 = document.getElementById("msg3");
+            div3.classList.remove("filha2");
+            div3.classList.add("filha0");
+            let div4 = document.getElementById("msg4");
+            div4.classList.remove("filha0");
+            div4.classList.add("filha2");
+            break;
+        }
+    }
+}
